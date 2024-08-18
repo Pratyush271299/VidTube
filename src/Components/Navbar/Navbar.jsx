2b68d10f99d15e98904c81fa25057a92d0e56617
Pratyush271299 <lightyagamikilledl@gmail.com>
2024-08-18 10:39:50 +0530

    adfa

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Navbar.css';
+import assets from '../../assets/assets'
+
+const Navbar = () => {
+  return (
+    <nav className='flex-div'>
+      <div className='nav-left flex-div'>
+        <img className='menu-icon' src={assets.menu_icon} alt="" />
+        <img className='logo' src={assets.logo} alt="" />
+      </div>
+      <div className="nav-middle flex-div">
+        <input type="text" placeholder='Search' />
+        <img src={assets.search} alt="" />
+      </div>
+      <div className="nav-right flex-div">
+        <img src={assets.upload} alt="" />
+        <img src={assets.more} alt="" />
+        <img src={assets.notification} alt="" />
+        <img src={assets.jack} className='user-icon' alt="" />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
